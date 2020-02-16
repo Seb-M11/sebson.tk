d2bbf962af2b6a06d6ba251e3e8ef5c9093778e1
Volper212 <volpergaming@gmail.com>
2020-02-16 18:40:22 +0100

    :sparkles: Add colorful chessboard

--- projekty/szachy/ts/index.ts
+++ projekty/szachy/ts/index.ts
@@ -1,5 +1,18 @@
 import VTag from './vtag';
 
-const div = new VTag('div');
-div.append(div, 8);
-VTag.select('.board').append(div, 8);
+const board = VTag.select('.board');
+
+board.append(new VTag('div', new VTag('div').multiply(8)).multiply(8));
+
+VTag.select('#light-squares-color').listen('input', ({ target }) => {
+  board.setCSSProperty(
+    '--light-squares-color',
+    (target as HTMLInputElement).value
+  );
+});
+VTag.select('#dark-squares-color').listen('input', ({ target }) => {
+  board.setCSSProperty(
+    '--dark-squares-color',
+    (target as HTMLInputElement).value
+  );
+});
--- projekty/szachy/ts/vtag.ts
+++ projekty/szachy/ts/vtag.ts
@@ -1,25 +1,49 @@
 export default class VTag {
-  element: Element;
+  readonly element: HTMLElement;
 
-  constructor(tagname: string);
-  constructor(element: Element);
-  constructor(tag: string | Element) {
-    this.element = typeof tag === 'string' ? document.createElement(tag) : tag;
+  constructor(tagname: keyof HTMLElementTagNameMap, children?: VTag[]);
+  constructor(element: HTMLElement, children?: VTag[]);
+  constructor(
+    element: keyof HTMLElementTagNameMap | HTMLElement,
+    children?: VTag[]
+  ) {
+    this.element =
+      typeof element === 'string' ? document.createElement(element) : element;
+    if (children) {
+      this.append(children);
+    }
   }
 
   static select(selector: string): VTag {
-    return new VTag(document.querySelector(selector));
+    return new VTag(document.querySelector(selector) as HTMLElement);
   }
 
-  append(child: VTag, count = 1): void {
-    if (count === 1) {
-      this.element.appendChild(child.element);
-    } else {
-      const children = document.createDocumentFragment();
-      for (let i = 0; i < count; ++i) {
-        children.appendChild(child.element.cloneNode(true));
-      }
-      this.element.appendChild(children);
+  /** Warning: This uses the cloneNode() method, which strips off event listeners */
+  multiply(count: number): VTag[] {
+    const vTags = [];
+    for (let i = 0; i < count; ++i) {
+      vTags[i] = new VTag(this.element.cloneNode(true) as HTMLElement);
     }
+    return vTags;
+  }
+
+  append(children: VTag[]): void {
+    this.element.append(...children.map(child => child.element));
+  }
+
+  listen<K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (event: HTMLElementEventMap[K]) => void,
+    options?: boolean | AddEventListenerOptions
+  ): void {
+    this.element.addEventListener(type, listener, options);
+  }
+
+  getCSSProperty(property: string): string {
+    return this.element.style.getPropertyValue(property);
+  }
+
+  setCSSProperty(property: string, value: string): void {
+    this.element.style.setProperty(property, value);
   }
 }
